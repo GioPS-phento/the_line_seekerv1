22865147678e7a9144a04f14e4fa2d5210496d53
GioPS-phento <gphidaninmortal16@gmail.com>
2026-08-05 22:55:28 -0500

    Add files via upload

---
+++ f1curveopticatcher.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 00:13:57 2026
+
+@author: GIO
+"""
+
+import duckdb
+import fastf1
+import pandas as pd
+import matplotlib.pyplot as plt
+
+curvas=['C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','C13','C14','C15','C16']
+dtcur={}
+trayectoria={}
+lapnum=[]
+j=0
+i=0
+fastf1.Cache.enable_cache('C:/Users/GIO/ETL')
+
+session = fastf1.get_session(2022, 'Silverstone', 'R')
+session.load(laps=True, telemetry=True, weather=False)
+laps = session.laps
+driver_laps = laps.pick_drivers('PER').reset_index(drop=True)
+print(f"Total de vueltas: {len(driver_laps)}")
+
+# Lista para acumular la telemetría de todas las vueltas
+all_telemetry = []
+masa = 798
+for _, lap in driver_laps.iterrows():
+    try:
+        tel = lap.get_telemetry().add_distance()
+        tel['Velocidad_ms'] = tel['Speed'] / 3.6
+        tel['Energia_cinetica_J'] = 0.5 * masa * tel['Velocidad_ms']**2
+        # Agregar columnas de identificación de la vuelta
+        tel['LapNumber'] = lap['LapNumber']
+        #tel['Distance'] = lap['Distance']
+        
+       
+        all_telemetry.append(tel[['LapNumber', 'Energia_cinetica_J','Distance','X','Y']])
+        
+    except Exception as e:
+        print(f"Vuelta {lap['LapNumber']} sin telemetría: {e}")
+
+full_telemetry = pd.concat(all_telemetry, ignore_index=True)
+
+#mean_kinectic=full_telemetry.groupby('LapNumber')['Energia_cinetica_J'].mean().reset_index()
+#duckdb.execute("CREATE TABLE mean_kinecticAS SELECT * FROM mean_kinectic")
+#duckdb.sql("SELECT * FROM mean_kinectic ORDER BY LapNumber").show()
+
+
+circuit_info = session.get_circuit_info()
+corners = circuit_info.corners
+
+def asignar_curva(distancia, corners):
+    for _, corner in corners.iterrows():
+        # Si la distancia está cerca del inicio de la curva
+        if abs(distancia - corner['Distance']) < 100:  # margen de 100m
+            return f"C{corner['Number']}"
+    return 'Recta'
+
+full_telemetry['Curva'] = full_telemetry['Distance'].apply(lambda d: asignar_curva(d, corners))
+q1 = full_telemetry['Energia_cinetica_J'].quantile(0.25)
+q3 = full_telemetry['Energia_cinetica_J'].quantile(0.75)
+IQR=q3-q1
+higher=q3+1.5*(IQR)
+lower=q1-1.5*(IQR)
+df_sin_outliers = full_telemetry[
+    (full_telemetry['Energia_cinetica_J'] >= lower) &
+    (full_telemetry['Energia_cinetica_J']  <= higher)
+    ]
+
+
+for curva in curvas:
+
+    dtcur[curva] = full_telemetry[full_telemetry['Curva'] == curva]
+
+
+for nombre, dtf in dtcur.items():
+    newdtf=dtf.groupby('LapNumber')['Energia_cinetica_J'].mean().reset_index()
+    lapnum.append(newdtf['Energia_cinetica_J'].idxmax())
+
+for cruva ,lap in zip(curvas,lapnum):
+        trayectoria[cruva]=full_telemetry[(full_telemetry['Curva'] == cruva) & (full_telemetry['LapNumber'] == lap)]
+for name, coords in trayectoria.items():
+    plt.plot(coords['X'],coords['Y'])
+    plt.axis('equal')
+plt.show()
+
